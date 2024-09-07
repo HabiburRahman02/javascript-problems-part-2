2ca30ea92e017a14c8d6efb505e458cc9cd76a67
Habibur Rahman <habibur88454@gmail.com>
2024-09-08 03:25:57 +0600

    added expensive phones

--- expensive.js
+++ expensive.js
@@ -3,19 +3,19 @@ const phones = [
     { name: 'oppo', price: 23000, color: 'black' },
     { name: 'vivo', price: 5000, color: 'black' },
     { name: 'Iphone', price: 166000, color: 'black' },
-    { name: 'realme', price: 3000, color: 'black' },
+    { name: 'realme', price: 34000, color: 'black' },
 ];
 
 
-function getCheapest() {
-    let cheap = phones[0];
+function getExpensive() {
+    let max = phones[0];
     for (const phone of phones) {
-        if(phone.price < cheap.price){
-            cheap = phone;
+        if(phone.price > max.price){
+            max = phone;
         }
     }
-    return cheap;
+    return max;
 }
 
-const cheap = getCheapest(phones);
-console.log('cheapest phone is: ', cheap);
+const expensive = getExpensive(phones);
+console.log('expensive phone is: ', expensive);
